22311b05d7a346b2ad87fc162235f5eeb7baa9f6
Steven Skoczen <steven@quantumimagery.com>
2020-11-11 15:53:29 +1300

    Updates staff_member_required to respect LOGIN_URL

--- django/contrib/admin/views/decorators.py
+++ django/contrib/admin/views/decorators.py
@@ -1,13 +1,17 @@
+from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import user_passes_test
 
 
 def staff_member_required(view_func=None, redirect_field_name=REDIRECT_FIELD_NAME,
-                          login_url='admin:login'):
+                          login_url=None):
     """
     Decorator for views that checks that the user is logged in and is a staff
     member, redirecting to the login page if necessary.
     """
+    if not login_url:
+        login_url = settings.LOGIN_URL or 'admin:login'
+
     actual_decorator = user_passes_test(
         lambda u: u.is_active and u.is_staff,
         login_url=login_url,
